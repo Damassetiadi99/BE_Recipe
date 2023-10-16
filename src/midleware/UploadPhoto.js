50cb3f998d6ab5c5f6fab89ecbdee4d5b7a61b98
damassetiadi99 <damassetiadi999@gmail.com>
2023-10-17 03:11:11 +0700

    "path"

--- src/midleware/UploadPhoto.js
+++ src/midleware/UploadPhoto.js
@@ -1,8 +1,8 @@
 const multer = require('multer')
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './tmp')
-      },
+    // destination: function (req, file, cb) {
+    //     cb(null, './tmp')
+    //   },
       filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
         cb(null, file.fieldname + '-' + uniqueSuffix + '.png')
